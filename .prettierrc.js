782d87b051d5e9b541c94a38675eee4707d8d69c
ferdeolazabal <ferdeolazabal@gmail.com>
2023-04-27 22:07:33 -0300

    Add initial config with Icons & navigator

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  jsxBracketSameLine: true,
+  printWidth: 100,
+  tabWidth: 4,
+  bracketSpacing: true,
+  bracketSameLine: true,
 };
